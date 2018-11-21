29bbf19ada134a482d9f5f7fccc3977bcd252c04
SAMUELWENDOLIN KETECHIE <samuelwendolin2@live.com>
2018-11-21 22:44:54 +0000

    initial api commit

---
+++ ApiControllers/MainController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackApi.ApiController
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class MainController : Controller
+    {
+        public MainController() { }
+
+        [HttpPost("upload/{image}")]
+        public ImageUploadResult PostImageToServer(string image)
+        {
+            var cloudinary = GetCloudinaryAccountInfo();
+
+            var imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"images/" + image);
+            var tranform = new Transformation().Crop("scale").Width(200).Height(200);
+            var eagerTransform = new List<Transformation>() {
+                new Transformation().Width(200).Height(200).Crop("thumb").Gravity("face").Radius(20).Effect("sepia"),
+                new Transformation().Width(100).Height(150).Crop("fit")
+            };
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(imagePath),
+                PublicId="cy_id",
+                Transformation = tranform,
+                EagerTransforms = eagerTransform,
+                Tags= "Khophi_"+image,
+                Folder="/web",
+                Overwrite=true,
+                Colors=true
+            };
+            var uploadResult = cloudinary.Upload(uploadParams);
+
+            return uploadResult;
+        }
+        
+
+        public Cloudinary GetCloudinaryAccountInfo()
+        {
+            var account = new Account
+            {
+                Cloud = "wendolin",
+                ApiKey = "447918465166282",
+                ApiSecret = "RCoyVQF9Gbxc6oNCNDfoNTK9-Nw"
+            };
+            var cloudinary = new Cloudinary(account);
+            return cloudinary;
+        }
+
+    }
+}
---
+++ Model/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackApi.Model
+{
+    public class Account
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+
+        public Account() { }
+
+        public Account(string cloudName,string apiKey,string apiSecret) {
+            CloudName = cloudName;
+            ApiKey = apiKey;
+            ApiSecret = ApiSecret;
+        }
+    }
+}
